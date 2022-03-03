a3e6f5ad921030a1a33e4313d1f4c9dc3d574278
hieu091099 <vai22806@gmail.com>
2022-03-03 11:57:10 +0700

    add some nessary libary

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
 export default function App() {
+  const [text, setText] = React.useState({
+    email: "",
+    password: ""
+  });
+  console.log(text)
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={{ backgroundColor: 'white' }}
+        label="Email"
+        value={text.email}
+        onChangeText={(value) => {
+          console.log(value);
+        }}
+      />
+      <TextInput
+        style={{ backgroundColor: 'white' }}
+        label="Password"
+        value={text.password}
+        onChangeText={(value) => {
+          console.log(value);
+        }}
+      />
     </View>
   );
 }

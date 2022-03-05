56ec8c39b9c3c8b01be8267e33a3693926bf18a9
trungnam-c <namn71201@gmail.com>
2022-03-05 07:44:26 +0700

    demo login

--- App.js
+++ App.js
@@ -1,43 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
-  const [text, setText] = React.useState({
+  const [text, setText] = useState({
     email: "",
     password: ""
   });
-  console.log(text)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ display: 'flex', alignItems: 'center' }}>
+      <View style={styles.logo}>
+        <Image source={require('./screen/image/LAI_logo.png')} />
+      </View>
       <TextInput
-        style={{ backgroundColor: 'white' }}
+        style={styles.inputlogin}
         label="Email"
         value={text.email}
-        onChangeText={(value) => {
-          console.log(value);
-        }}
+        onChangeText={text => setText({ ...text, email: text })}
       />
       <TextInput
-        style={{ backgroundColor: 'white' }}
+        style={styles.inputlogin}
         label="Password"
         value={text.password}
-        onChangeText={(value) => {
-          console.log(value);
-        }}
+        onChangeText={text => setText({ ...text, password: text })}
       />
+      <Button style={styles.inputlogin} icon="login" mode="contained" onPress={() => console.log('Pressed')}>
+        Đăng Nhập
+      </Button>
+      <View style={{ width: 100, height: 100, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={() => {
+          console.log('press ');
+        }}>
+          <Ionicons name="ios-finger-print-outline" size={50} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  logo: {
+    height: '40%',
+    display: 'flex',
+    justifyContent: 'center'
   },
+  inputlogin: {
+    marginVertical: 10,
+    width: '90%'
+  }
 });
